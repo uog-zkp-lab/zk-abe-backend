aa84f92f14e4b44eb8080cf4a9fe18a2b8a4ff9b
Shivani Jamwal <s.shivani.1@research.gla.ac.uk>
2024-07-01 16:16:54 +0530

    test policy language attempt.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use warp::Filter;
 use serde::{Deserialize, Serialize};
+use rabe::schemes::bsw::*;
+use rabe::utils::policy::pest::PolicyLanguage;
 
 #[derive(Deserialize, Serialize)]
 struct Response {
@@ -15,10 +17,32 @@ async fn main() {
             })
         });
 
-    warp::serve(hello)
-        .run(([127, 0, 0, 1], 3030))
+    let test_policy = warp::path!("test_policy")
+        .map(|| {
+            // Initialize the BSW CP-ABE scheme
+            let (pk, msk) = setup();
+            let plaintext = b"Hello, World!";
+
+            // Define a policy
+            let policy = String::from(r#""A" and "B""#);
+            let ct_cp = encrypt(&pk, &policy, PolicyLanguage::HumanPolicy, &*plaintext).unwrap(); // Use dereference operator here
+
+            // Generate a key pair
+            let sk = keygen(&pk, &msk, &vec!["A", "B"]).unwrap();
+            let decrypted_plaintext = decrypt(&sk, &ct_cp).unwrap();
+
+            let message = if decrypted_plaintext == plaintext {
+                "Policy language test passed!"
+            } else {
+                "Policy language test failed."
+            };
+
+            warp::reply::json(&Response {
+                message: message.to_string(),
+            })
+        });
+
+    warp::serve(hello.or(test_policy))
+        .run(([127, 0, 0, 1], 3031))
         .await;
-
-        println!("Hello, world!");
 }
-
